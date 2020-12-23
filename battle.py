3f6184c433d8f04c0d8402bf3c0aa5f4e295b14d
Shane Claussen <sclaussen@yahoo.com>
2020-12-23 12:37:49 -0800

    Automated the battle

--- battle.py
+++ battle.py
@@ -1,19 +1,66 @@
+# Add in multiple bot mobs
+# Randomize who attacks first vs second
+# Randomize who heals first vs second
 from Guardian import *
 from Lynel import *
 from Ganon import *
 from Link import *
 
-options = {
-    "Gold Lynel 1": GoldLynel("Gold-Lynel-1"),
-    "Gold Lynel 2": GoldLynel("Gold-Lynel-2"),
-    "Silver Lynel 1": SilverLynel("Silver-Lynel-1"),
-    "Silver Lynel 2": SilverLynel("Silver-Lynel-2"),
-    "Silver Lynel 3": SilverLynel("Silver-Lynel-3"),
-    "Guardian 1": Guardian("Guardian-1"),
-    "Guardian 2": Guardian("Guardian-2"),
-    "Ganon": Ganon("Ganon"),
-    "Link": Link("Link")
-}
+def battle():
+
+    characters = {
+        "Gold Lynel 1": GoldLynel("Gold-Lynel-1"),
+        "Gold Lynel 2": GoldLynel("Gold-Lynel-2"),
+        "Silver Lynel 1": SilverLynel("Silver-Lynel-1"),
+        "Silver Lynel 2": SilverLynel("Silver-Lynel-2"),
+        "Silver Lynel 3": SilverLynel("Silver-Lynel-3"),
+        "Guardian 1": Guardian("Guardian-1"),
+        "Guardian 2": Guardian("Guardian-2"),
+        "Ganon": Ganon("Ganon"),
+        "Link": Link("Link")
+    }
+
+    # Prompt for (or randomly select) the user
+    # user_key = prompt("Select a player", "Try again, that is not a valid option.", characters)
+    # user = characters[user_key]
+    user = random.choice(list(characters.values()))
+    # characters.pop(user_key)
+
+    # Prompt for (or randomly select) the bot
+    # bot_key = prompt("Select a bot", "Try again, that is not a valid option.", characters)
+    # bot = characters[bot_key]
+    bot = random.choice(list(characters.values()))
+
+    print("Battle: " + str(user) + " (you) vs. " + str(bot))
+    while True:
+        print("-" * 40)
+
+        # attack_key = prompt("Choose an attack", "Try again, that is not a valid attack.", user.attacks.keys())
+        if (chance(50)):
+            attack_key = random.choice(list(user.attacks.keys()))
+            user.attacks[attack_key](bot)
+            if bot.health <= 0:
+                print(bot.name + ' died.')
+                break
+
+        if (chance(50)):
+            attack_function = random.choice(list(bot.attacks.values()))
+            attack_function(user)
+            if user.health <= 0:
+                print(user.name + ' died.')
+                break
+
+        if (chance(40)):
+            user.heal()
+
+        if (chance(40)):
+            bot.heal()
+
+    print("-" * 40)
+    if user.health <= 0:
+        print("You lose!")
+    else:
+        print("You win!")
 
 def prompt(prefix, error, options):
     print(prefix + ": ")
@@ -46,49 +93,5 @@ def chance(percent):
     if (random.randint(0, 100) <= percent):
         return True
     return False
-
-def battle():
-
-    # Prompt the player for which player they want to use
-    user_key = prompt("Choose a player", "Try again, that is not a valid player.", options)
-    user = options[user_key]
-    options.pop(user_key)
-
-    # Randomly pick a bot from the remaining characters
-    bot = random.choice(list(options.values()))
-
-    print("Battle: " + str(user) + " (you) vs. " + str(bot))
-    print()
-    while True:
-        print("-" * 40)
-
-        # attack_key = prompt("Choose an attack", "Try again, that is not a valid attack.", user.attacks.keys())
-        if (chance(50)):
-            attack_key = random.choice(list(user.attacks.keys()))
-            user.attacks[attack_key](bot)
-            if bot.health <= 0:
-                print(bot.name + ' died.')
-                break
-
-        if (chance(50)):
-            attack_function = random.choice(list(bot.attacks.values()))
-            attack_function(user)
-            if user.health <= 0:
-                print(user.name + ' died.')
-                break
-
-        if (chance(40)):
-            user.heal()
-
-        if (chance(30)):
-            bot.heal()
-
-    print("-" * 40)
-    if user.health <= 0:
-        print("You lose!")
-    else:
-        print("You win!")
-
-
 
 battle()
