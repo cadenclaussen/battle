b86ed31df1d2370e9fba78bccb3ba777dfa3045a
Shane Claussen <sclaussen@yahoo.com>
2020-12-23 12:17:56 -0800

    Automated the attack, fixed up attack/heal output

--- Mob.py
+++ Mob.py
@@ -16,17 +16,16 @@ class Mob():
 
     def attack(self, attack_name, target, damage):
         target.health -= damage
-        print(self.name + " performed a " + attack_name + " attack on " + target.name + " doing " + str(damage) + " points of damage.")
+        if (target.health < 0):
+            target.health = 0
+        print(self.name + " attacked " + target.name + " using the " + attack_name + " attack (" + str(damage) + " damage) (" + target.name + " " + str(target.health) + ").")
 
     def heal(self):
         amount = random.randint(1, 5) * self.heal_multiplier
         self.health += amount
         if (self.health > self.initial_health):
             self.health = self.initial_health
-        print(self.name + " healed " + str(amount) + " points of damage.")
-
-    def status(self):
-        print(self.name + "'s health is " + str(self.health) + "points.")
+        print(self.name + " healed (" + str(amount) + "/" + str(self.health) + ").")
 
     def __str__(self):
         return self.name + " (" + str(self.health) + ")"
--- battle.py
+++ battle.py
@@ -4,13 +4,13 @@ from Ganon import *
 from Link import *
 
 options = {
-    "Gold Lynel 1": GoldLynel("Gold Lynel 1"),
-    "Gold Lynel 2": GoldLynel("Gold Lynel 2"),
-    "Silver Lynel 1": SilverLynel("Silver Lynel 1"),
-    "Silver Lynel 2": SilverLynel("Silver Lynel 2"),
-    "Silver Lynel 3": SilverLynel("Silver Lynel 3"),
-    "Guardian 1": Guardian("Guardian 1"),
-    "Guardian 2": Guardian("Guardian 2"),
+    "Gold Lynel 1": GoldLynel("Gold-Lynel-1"),
+    "Gold Lynel 2": GoldLynel("Gold-Lynel-2"),
+    "Silver Lynel 1": SilverLynel("Silver-Lynel-1"),
+    "Silver Lynel 2": SilverLynel("Silver-Lynel-2"),
+    "Silver Lynel 3": SilverLynel("Silver-Lynel-3"),
+    "Guardian 1": Guardian("Guardian-1"),
+    "Guardian 2": Guardian("Guardian-2"),
     "Ganon": Ganon("Ganon"),
     "Link": Link("Link")
 }
@@ -43,7 +43,7 @@ def prompt(prefix, error, options):
     return option_numbers[number]
 
 def chance(percent):
-    if (random.randint(0, 100) > (100 - percent)):
+    if (random.randint(0, 100) <= percent):
         return True
     return False
 
@@ -57,35 +57,32 @@ def battle():
     # Randomly pick a bot from the remaining characters
     bot = random.choice(list(options.values()))
 
+    print("Battle: " + str(user) + " (you) vs. " + str(bot))
     print()
     while True:
-        print()
         print("-" * 40)
-        print("Battle: " + str(user) + " (you) vs. " + str(bot))
 
-        print()
-        attack_key = prompt("Choose an attack", "Try again, that is not a valid attack.", user.attacks.keys())
-        print()
-        user.attacks[attack_key](bot)
-        if bot.health <= 0:
-            break
-
-        attack_function = random.choice(list(bot.attacks.values()))
-        attack_function(user)
-        if user.health <= 0:
-            break
-
-        print("Result: " + str(user) + " (you) vs. " + str(bot))
-
-        print()
+        # attack_key = prompt("Choose an attack", "Try again, that is not a valid attack.", user.attacks.keys())
         if (chance(50)):
+            attack_key = random.choice(list(user.attacks.keys()))
+            user.attacks[attack_key](bot)
+            if bot.health <= 0:
+                print(bot.name + ' died.')
+                break
+
+        if (chance(50)):
+            attack_function = random.choice(list(bot.attacks.values()))
+            attack_function(user)
+            if user.health <= 0:
+                print(user.name + ' died.')
+                break
+
+        if (chance(40)):
             user.heal()
-        if (chance(80)):
+
+        if (chance(30)):
             bot.heal()
 
-        print("Result: " + str(user) + " (you) vs. " + str(bot))
-
-    print()
     print("-" * 40)
     if user.health <= 0:
         print("You lose!")
